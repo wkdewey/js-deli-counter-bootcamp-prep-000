32ef0baafbaa89288c6a2c56fe4eabc7e3b92ed2
wkdewey@gmail.com <wkdewey@gmail.com>
2018-05-27 19:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ var katzDeliLine = [];
 
 function takeANumber(line, newCustomer) {
   line.push(newCustomer);
-  var position = line.length
-  return `Welcome, ${newCustomer}. You are number ${position} in line.`
+  var position = line.length;
+  return `Welcome, ${newCustomer}. You are number ${position} in line.`;
 }
 
 function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-  return line.shift();
+  var name = line.shift();
+  return `Currently serving ${name}.`;
   }
 }
